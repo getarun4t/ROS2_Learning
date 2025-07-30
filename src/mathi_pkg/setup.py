dd2cf52835dc96b56b8a4624a47fa19c96e63635
Arun Sasi <aruntdpa@gmail.com>
2025-07-30 13:45:29 +0200

    Adding object oriented client

--- src/mathi_pkg/setup.py
+++ src/mathi_pkg/setup.py
@@ -26,7 +26,8 @@ setup(
             "number_publisher = mathi_pkg.number_publisher:main",
             "number_counter = mathi_pkg.number_counter:main",
             "add_two_ints_server = mathi_pkg.add_two_ints_server:main",
-            "add_two_ints_client_no_oop = mathi_pkg.add_two_ints_client_no_oop:main"
+            "add_two_ints_client_no_oop = mathi_pkg.add_two_ints_client_no_oop:main",
+            "add_two_ints_client = mathi_pkg.add_two_ints_client:main"
         ],
     },
 )
