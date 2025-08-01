295b7a89308a9211988f712847e3355da7dadbfa
Arun Sasi <38099892+getarun4t@users.noreply.github.com>
2025-08-01 15:48:44 +0200

    Parameters (#6)
    * Python parameter demo
    
    * Adding C++ parameter demo

--- src/chala_pkg/src/c++ template.cpp
+++ src/chala_pkg/src/c++ template.cpp
@@ -1,9 +1,9 @@
 #include "rclcpp/rclcpp.hpp"
  
-class MyCustomNode : public rclcpp::Node // MODIFY NAME
+class MyCustomNode : public rclcpp::Node //MODIFY_NAME
 {
 public:
-    MyCustomNode() : Node("node_name") // MODIFY NAME
+    MyCustomNode() : Node("node_name") //MODIFY_NAME
     {
     }
  
@@ -13,7 +13,7 @@ private:
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<MyCustomNode>(); // MODIFY NAME
+    auto node = std::make_shared<MyCustomNode>(); //MODIFY_NAME
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
