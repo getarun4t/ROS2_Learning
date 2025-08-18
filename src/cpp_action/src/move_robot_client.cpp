c21869b0b8715af28e4d6cc653f6a2f18bf9bdda
Arun Sasi <aruntdpa@gmail.com>
2025-08-18 15:12:05 +0200

    FIxing issues

--- src/cpp_action/src/move_robot_client.cpp
+++ src/cpp_action/src/move_robot_client.cpp
@@ -1,12 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "my_robot_interfaces/action/move_robot.hpp"
-#include "example_interfaces/msg/empty.hpp"
+//#include "example_interfaces/msg/empty.hpp"
  
 using MoveRobot = my_robot_interfaces::action::MoveRobot;
 using MoveRobotGoalHandle = rclcpp_action::ClientGoalHandle<MoveRobot>;
 using namespace std::placeholders;
-using Empty = example_interfaces::msg::Empty;
+//using Empty = example_interfaces::msg::Empty;
 
 class MoveRobotClientNode : public rclcpp::Node 
 {
@@ -17,12 +17,12 @@ public:
             this, 
             "count_until");
 
-        cancel_subscriber_ = create_subscription<Empty>(
+  /*      cancel_subscriber_ = create_subscription<Empty>(
             "cancel_move", 
             10, 
             std::bind(&MoveRobotClientNode::callback_cancel_move), 
             this, 
-            _1);
+            _1);*/
     }
 
     void send_goal(int target_position, int velocity){
@@ -42,16 +42,13 @@ public:
         //Send the goal
         RCLCPP_INFO(get_logger(), "Sending the goal");
         move_robot_client_->async_send_goal(goal, options);
-        
-        //Cancel goal test
-        //timer_ = this->create_wall_timer(std::chrono::seconds(2), std::bind(&MoveRobotClientNode::timer_callback, this));
     }
  
 private:
     rclcpp_action::Client<MoveRobot>::SharedPtr move_robot_client_;
     rclcpp::TimerBase::SharedPtr timer_;
     MoveRobotGoalHandle::SharedPtr goal_handle_;
-    rclcpp::Subscription<MoveRobot>::SharedPtr cancel_subscriber_;
+  //  rclcpp::Subscription<MoveRobot>::SharedPtr cancel_subscriber_;
 
     void timer_callback(){
         RCLCPP_INFO(get_logger(), "Cancel the goal");
@@ -73,8 +70,8 @@ private:
             RCLCPP_WARN(get_logger(), "Goal Cancelled");
         }
 
-        RCLCPP_INFO(get_logger(), "Position : %d", result.result->position);
-        RCLCPP_INFO(get_logger(), "Message : %d", result.result->message);
+        int position = result.result->position;
+        RCLCPP_INFO(get_logger(), "Position : %d", position);
     }
 
     //Callback to know if the goal is accepted or rejected
@@ -94,7 +91,7 @@ private:
         int current_position = feedback->current_position;
         RCLCPP_INFO(get_logger(), "Feedback : %d", current_position);
     }
-
+/*
     void callback_cancel_move(const Empty::SharedPtr msg){
         (void)msg;
         cancel_goal();
@@ -105,7 +102,7 @@ private:
             move_robot_client_->async_cancel_goal(goal_handle_);
             goal_handle_.reset();
         }
-    }
+    }*/
 };
  
 int main(int argc, char **argv)
