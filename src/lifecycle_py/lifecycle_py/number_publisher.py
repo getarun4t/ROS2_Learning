8a6ca17b71c55ba1c92432b4b48530fc7702e80b
Arun Sasi <aruntdpa@gmail.com>
2025-08-19 10:38:33 +0200

    Adding base files, editing py - add on_configure, on_cleanup

--- src/lifecycle_py/lifecycle_py/number_publisher.py
+++ src/lifecycle_py/lifecycle_py/number_publisher.py
@@ -2,60 +2,29 @@
 import rclpy
 from rclpy.lifecycle import LifecycleNode
 from rclpy.lifecycle.node import LifecycleState, TransitionCallbackReturn
-from example_interfaces.msg import Int64
 
+from example_interfaces.msg import Int64
 
 class NumberPublisherNode(LifecycleNode):
     def __init__(self):
         super().__init__("number_publisher")
-        self.get_logger().info("IN constructor")
+        self.get_logger().info("In Constructor")
         self.number_ = 1
         self.publish_frequency_ = 1.0
-        self.number_publisher_ = None
-        self.number_timer_ = None
-        
-    # Create ROS2 communications, connect to HW
-    def on_configure(self, previous_state: LifecycleState):
-        self.get_logger().info("IN on_configure")
+
+    # Create ROS2 communications, connect to HW etc.
+    def on_configure(self, state: LifecycleState):
+        self.get_logger().info("In On Configure")
         self.number_publisher_ = self.create_lifecycle_publisher(Int64, "number", 10)
-        self.number_timer_ = self.create_timer(
-            1.0 / self.publish_frequency_, self.publish_number)
-        self.number_timer_.cancel()
+        self.number_timer_ = self.create_timer(1.0 / self.publish_frequency_, self.publish_number)
         return TransitionCallbackReturn.SUCCESS
 
-    # Activate/Enable HW
-    def on_activate(self, previous_state: LifecycleState):
-        self.get_logger().info("IN on_activate")
-        self.number_timer_.reset()
-        return super().on_activate(previous_state)
-
-    # Deactivate/Disable HW
-    def on_deactivate(self, previous_state: LifecycleState):
-        self.get_logger().info("IN on_deactivate")
-        self.number_timer_.cancel()
-        return super().on_deactivate(previous_state)
-
-    # Destroy ROS2 communications, disconnect from HW
-    def on_cleanup(self, previous_state: LifecycleState):
-        self.get_logger().info("IN on_cleanup")
+    # Destroy ROS2 communications, disconnect HW etc.
+    def on_cleanup(self, state: LifecycleState):
+        self.get_logger().info("In On CleanUp")
         self.destroy_lifecycle_publisher(self.number_publisher_)
         self.destroy_timer(self.number_timer_)
         return TransitionCallbackReturn.SUCCESS
-    
-    # Cleanup everything
-    def on_shutdown(self, previous_state: LifecycleState):
-        self.get_logger().info("IN on_shutdown")
-        self.destroy_lifecycle_publisher(self.number_publisher_)
-        self.destroy_timer(self.number_timer_)
-        return TransitionCallbackReturn.SUCCESS
-    
-    # Process errors, deactivate + cleanup
-    def on_error(self, previous_state: LifecycleState):
-        self.get_logger().info("IN on_error")
-        self.destroy_lifecycle_publisher(self.number_publisher_)
-        self.destroy_timer(self.number_timer_)
-        # do some checks, if ok, then return SUCCESS, if not FAILURE
-        return TransitionCallbackReturn.FAILURE
 
     def publish_number(self):
         msg = Int64()
--- src/lifecycle_py/setup.py
+++ src/lifecycle_py/setup.py
@@ -20,8 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "number_publisher = lifecycle_py.number_publisher:main",
-            "lifecycle_node_manager = lifecycle_py.lifecycle_node_manager:main"
+            "number_publisher = lifecycle_py.number_publisher:main"
         ],
     },
 )
