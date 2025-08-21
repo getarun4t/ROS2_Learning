a755c2620128e226ac86dae00bcaed3cafa19726
Arun Sasi <aruntdpa@gmail.com>
2025-08-21 16:49:09 +0200

    Control turtle with life cyle node

--- src/finale/src/turtle_controller.cpp
+++ src/finale/src/turtle_controller.cpp
@@ -1,35 +1,50 @@
 #include "rclcpp/rclcpp.hpp"
 #include "turtlesim/srv/kill.hpp"
 #include "turtlesim/srv/spawn.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "my_robot_interfaces/action/move_turtle.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using Spawn = turtlesim::srv::Spawn;
 using Kill = turtlesim::srv::Kill;
+using Twist = geometry_msgs::msg::Twist;
+using MoveTurtle = my_robot_interfaces::action::MoveTurtle;
+using MoveTurtleGoalHandle = rclcpp_action::ServerGoalHandle<MoveTurtle>;
+
+using namespace std::placeholders;
+using namespace std::chrono_literals;
 
 class TurtleController : public rclcpp::Node 
 {
 public:
     TurtleController() : Node("turtle_controller") 
     {
-        declare_parameter(
-            "turtle_name", 
-            rclcpp::PARAMETER_STRING
-        );
-        turtle_name_ = get_parameter("turtle_name").as_string();
+        this->declare_parameter("turtle_name", "aama");
+        turtle_name_ = this->get_parameter("turtle_name").as_string();
 
         cb_group_ = create_callback_group(rclcpp::CallbackGroupType::Reentrant);
 
-        spawn_turtle_client_ = create_client<Spawn>(
-            "/spawn",
-            rclcpp::ServicesQoS(),
-            cb_group_
-        );
-        kill_turtle_client_ = create_client<Kill>(
-            "/kill",
-            rclcpp::ServicesQoS(),
+        spawn_turtle_client_ = this->create_client<Spawn>(
+            "/spawn", rclcpp::ServicesQoS(), cb_group_);
+
+        kill_turtle_client_ = this->create_client<Kill>(
+            "/kill", rclcpp::ServicesQoS(), cb_group_);
+
+        cmd_vel_publisher_ = create_publisher<Twist>("/" + turtle_name_ + "/cmd_vel", 10);
+
+        spawn_turtle_thread_ = std::thread(std::bind(&TurtleController::spawn_turtle, this));
+
+        move_turtle_server_ = rclcpp_action::create_server<MoveTurtle>(
+            this,
+            "move_turtle_" + turtle_name_,
+            std::bind(&TurtleController::goal_callback, this, _1, _2),
+            std::bind(&TurtleController::cancel_callback, this, _1),
+            std::bind(&TurtleController::handle_accepted_callback, this, _1),
+            rcl_action_server_get_default_options(),
             cb_group_
         );
 
-        spawn_turtle_thread_ = std::thread(std::bind(&TurtleController::spawn_turtle, this));
+        RCLCPP_INFO(get_logger(), "Action has been started");
     }
  
 private:
@@ -38,9 +53,92 @@ private:
     rclcpp::Client<Spawn>::SharedPtr spawn_turtle_client_;
     rclcpp::Client<Kill>::SharedPtr kill_turtle_client_;
     rclcpp::CallbackGroup::SharedPtr cb_group_;
+    rclcpp_action::Server<MoveTurtle>::SharedPtr move_turtle_server_;
+    rclcpp::Publisher<Twist>::SharedPtr cmd_vel_publisher_;
+    std::shared_ptr<MoveTurtleGoalHandle> goal_handle_;
+    std::mutex mutex_;
 
     std::thread spawn_turtle_thread_;
     std::thread kill_turtle_thread_;
+
+    rclcpp_action::GoalResponse goal_callback(
+        const rclcpp_action::GoalUUID &uuid,
+        std::shared_ptr<const MoveTurtle::Goal> goal){
+            (void) uuid;
+            RCLCPP_INFO(get_logger(), "Received a new goal");
+
+            //Goal policy
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+                if ((goal_handle_) && goal_handle_->is_active()){
+                    RCLCPP_WARN(get_logger(), "Current goal still active");
+                    return rclcpp_action::GoalResponse::REJECT;
+                }
+            }
+
+            //Validate the new goal
+            if (fabs(goal->linear_vel_x) > 3.0 ||
+                fabs(goal->angular_vel_z) > 3.0 ||
+                (goal->duration_sec <=0.0)){
+                    RCLCPP_WARN(get_logger(), "Invalid goal");
+                    return rclcpp_action::GoalResponse::REJECT;
+                }
+
+            RCLCPP_INFO(get_logger(), "Accepted a new goal");
+            return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+        }
+
+    rclcpp_action::CancelResponse cancel_callback(
+        const std::shared_ptr<MoveTurtleGoalHandle> goal_handle){
+            (void) goal_handle;
+            RCLCPP_INFO(get_logger(), "Received the cancelRequest");
+            return rclcpp_action::CancelResponse::ACCEPT;
+        }
+
+    void handle_accepted_callback(
+        const std::shared_ptr<MoveTurtleGoalHandle> goal_handle){
+            RCLCPP_INFO(get_logger(), "Executing the goal");
+            execute_goal(goal_handle);
+        }
+
+    void execute_goal(
+        const std::shared_ptr<MoveTurtleGoalHandle> goal_handle){
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+                goal_handle_ = goal_handle;
+            }
+
+            double linear_vel_x = goal_handle->get_goal()->linear_vel_x;
+            double angular_vel_z = goal_handle->get_goal()->angular_vel_z;
+            int duration_sec = goal_handle->get_goal()->duration_sec;
+
+            auto result = std::make_shared<MoveTurtle::Result>();
+            auto start = now();
+            auto duration = rclcpp::Duration(duration_sec, 0);
+
+            rclcpp::Rate rate(10);
+
+            while(rclcpp::ok()){
+                if(now() - start > duration){
+                    auto msg = Twist();
+                    msg.linear.x = 0;
+                    msg.angular.z = 0;
+                    cmd_vel_publisher_->publish(msg);
+
+                    result->success = true;
+                    result-> message = "Success";
+                    goal_handle->succeed(result);
+                    return; 
+                }
+
+                auto msg = Twist();
+                msg.linear.x = linear_vel_x;
+                msg.angular.z = angular_vel_z;
+                rate.sleep();
+
+                cmd_vel_publisher_->publish(msg);
+            }
+        }
 
     void spawn_turtle(){
         spawn_turtle_client_->wait_for_service();
@@ -55,10 +153,6 @@ private:
         auto result = spawn_turtle_client_->async_send_request(request);
 
         RCLCPP_INFO(get_logger(), "Spawned turle : %s", result.get()->name.c_str());
-
-        std::this_thread::sleep_for(std::chrono::seconds(3));
-        kill_turtle_thread_ = std::thread(std::bind(&TurtleController::kill_turtle, this));
-
     }
 
     void kill_turtle(){
